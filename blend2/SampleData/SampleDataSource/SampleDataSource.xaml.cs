c44861bcdd098ffeb515e143d4e4b09f8a21979a
zzcl <zzcl@DESKTOP-MS80JMJ>
2018-09-25 19:48:23 +0800

    一 样式完成

---
+++ blend2/SampleData/SampleDataSource/SampleDataSource.xaml.cs
@@ -0,0 +1,124 @@
+﻿//   *********  请勿修改此文件   *********
+//   此文件由设计工具再生成。更改
+//   此文件可能会导致错误。
+namespace Expression.Blend.SampleData.SampleDataSource
+{
+    using System; 
+    using System.ComponentModel;
+
+// 若要在生产应用程序中显著减小示例数据涉及面，则可以设置
+// DISABLE_SAMPLE_DATA 条件编译常量并在运行时禁用示例数据。
+#if DISABLE_SAMPLE_DATA
+    internal class SampleDataSource { }
+#else
+
+    public class SampleDataSource : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public SampleDataSource()
+        {
+            try
+            {
+                Uri resourceUri = new Uri("/blend2;component/SampleData/SampleDataSource/SampleDataSource.xaml", UriKind.RelativeOrAbsolute);
+                System.Windows.Application.LoadComponent(this, resourceUri);
+            }
+            catch
+            {
+            }
+        }
+
+        private Persons _Persons = new Persons();
+
+        public Persons Persons
+        {
+            get
+            {
+                return this._Persons;
+            }
+        }
+    }
+
+    public class PersonsItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private string _Name = string.Empty;
+
+        public string Name
+        {
+            get
+            {
+                return this._Name;
+            }
+
+            set
+            {
+                if (this._Name != value)
+                {
+                    this._Name = value;
+                    this.OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        private bool _Sex = false;
+
+        public bool Sex
+        {
+            get
+            {
+                return this._Sex;
+            }
+
+            set
+            {
+                if (this._Sex != value)
+                {
+                    this._Sex = value;
+                    this.OnPropertyChanged("Sex");
+                }
+            }
+        }
+
+        private string _description = string.Empty;
+
+        public string description
+        {
+            get
+            {
+                return this._description;
+            }
+
+            set
+            {
+                if (this._description != value)
+                {
+                    this._description = value;
+                    this.OnPropertyChanged("description");
+                }
+            }
+        }
+    }
+
+    public class Persons : System.Collections.ObjectModel.ObservableCollection<PersonsItem>
+    { 
+    }
+#endif
+}
